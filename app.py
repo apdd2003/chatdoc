0b9e126a571726e40c25edd66d0b1eda6c419bb2
sayeemuddin <apdd2003@yahoo.co.in>
2024-05-20 18:24:12 +0530

    add basicauth and ratelimit features

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, current_app, make_response, session
+from flask import Flask, render_template, redirect, url_for, flash, request, current_app, make_response, session, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
 from utils import chunk_embed, askdoc
 import uuid
-
+from dotenv import load_dotenv
+load_dotenv()
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx'}
@@ -11,12 +16,35 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+auth = HTTPBasicAuth()
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
+
+
+
+users = {
+    'shaikh': generate_password_hash(os.getenv('password')),
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+    
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/',methods=['GET', 'POST'])
+@auth.login_required
+@limiter.limit("20/day", override_defaults=False)
 def home():
     session['uid'] = str(uuid.uuid4())
     query = request.args.get('query')
@@ -41,6 +69,7 @@ def home():
     return render_template("home.html")
 
 @app.route('/chat')
+@limiter.limit("10/day")
 def chat():
     query = request.args.get('query')
     file_name  = session['file_name'] 
